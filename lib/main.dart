0b7faff84ac0649bdc150fdba2175ef3c5693113
Sanjiv <115912003+sanchick02@users.noreply.github.com>
2024-04-01 23:21:11 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,24 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:green/firebase_options.dart';
-import 'package:green/screens/auth/auth_screen.dart';
-import 'package:green/screens/auth/login_form.dart';
-import 'package:green/screens/checkout/checkout_screen.dart';
-import 'package:green/screens/checkout/payment_screen.dart';
-import 'package:green/screens/checkout/receipt_screen.dart';
+import 'package:green/data/destinations.dart';
+import 'package:green/data/upcoming_trips.dart';
+import 'package:green/model/destination_model.dart';
+import 'package:green/screens/chatbot_screen.dart';
+import 'package:green/screens/destination/destination_details_screen.dart';
+import 'package:green/screens/eastwest_stats_screen.dart';
 import 'package:green/screens/home_screen.dart';
-import 'package:green/screens/landing_screen.dart';
-import 'package:green/screens/landing_screen1.dart';
-import 'package:green/screens/question_screen.dart';
+import 'package:green/screens/itinerary/saved_itinerary_screen.dart';
+import 'package:green/screens/itinerary/swipe_itinerary_screen.dart';
+import 'package:green/screens/landing/landing_screen.dart';
+import 'package:green/screens/landing/landing_screen1.dart';
+import 'package:green/screens/itinerary/my_itinerary_screen.dart';
+import 'package:green/screens/begreen_community_screen.dart';
+import 'package:green/screens/profile_screen.dart';
+import 'package:green/screens/landing/question_screen.dart';
 import 'package:green/data/questions.dart';
+import 'package:green/screens/upcomingtrip_details_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(
-    const MyApp(),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -29,9 +26,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CheckoutScreen(),
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      // home: DestinationDetailsScreen(
+      //   detail: flightDetailList[0],
+      // ),
+      // home: UpcomingTripDetailsScreen(
+      //   upcomingTrip: upcomingTripList[0],
+      // ),
+      // home: SwipeItineraryScreen(),
+      // home: SavedItineraryScreen(),
+      // home: MyItineraryScreen(),
+      // home: ProfileScreen(),
+      // home: ChatbotScreen(),
+      // home: HomeScreen(),
+      // home: QuestionsScreen(
+      //   questions: questionsList,
+      // ),
+      // home: BeGreenCommunityScreen(),
+      // home: LandingScreen1(),
+      home: EastWestStatsScreen(),
     );
   }
 }
