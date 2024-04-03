b7aa5c8d18d8bd87dafc6b6fd735b9b16d908439
Sanjiv <115912003+sanchick02@users.noreply.github.com>
2024-04-03 22:43:09 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:green/auth_widget_tree.dart';
 import 'package:green/data/destinations.dart';
 import 'package:green/data/upcoming_trips.dart';
 import 'package:green/model/destination_model.dart';
+import 'package:green/provider/user_provider.dart';
 import 'package:green/screens/chatbot_screen.dart';
-import 'package:green/screens/checkout/checkout_screen.dart';
-import 'package:green/screens/checkout/payment_screen.dart';
 import 'package:green/screens/destination/destination_details_screen.dart';
 import 'package:green/screens/eastwest_stats_screen.dart';
-import 'package:green/screens/explore/explore_destinations_screen.dart';
 import 'package:green/screens/home_screen.dart';
 import 'package:green/screens/itinerary/saved_itinerary_screen.dart';
 import 'package:green/screens/itinerary/swipe_itinerary_screen.dart';
@@ -19,9 +18,19 @@ import 'package:green/screens/profile_screen.dart';
 import 'package:green/screens/landing/question_screen.dart';
 import 'package:green/data/questions.dart';
 import 'package:green/screens/upcomingtrip_details_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -29,28 +38,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // home: DestinationDetailsScreen(
-      //   detail: flightDetailList[0],
-      // ),
-      // home: UpcomingTripDetailsScreen(
-      //   upcomingTrip: upcomingTripList[0],
-      // ),
-      // home: SwipeItineraryScreen(),
-      // home: SavedItineraryScreen(),
-      // home: MyItineraryScreen(),
-      // home: ProfileScreen(),
-      // home: ChatbotScreen(),
-      // home: HomeScreen(),
-      // home: QuestionsScreen(
-      //   questions: questionsList,
-      // ),
-      // home: BeGreenCommunityScreen(),
-      // home: LandingScreen1(),
-      // home: EastWestStatsScreen(),
-      // home: ExploreDestinationsScreen(),
-      home: PaymentScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: UserProvider(),
+        ),
+      ],
+      child: const MaterialApp(
+        home: AuthWidgetTree(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
