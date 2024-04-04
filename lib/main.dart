75127d78647012a744a6e49dbe5f7b9d5bf6bee4
Sanjiv <115912003+sanchick02@users.noreply.github.com>
2024-04-05 01:59:58 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,42 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:green/auth_widget_tree.dart';
 import 'package:green/data/destinations.dart';
 import 'package:green/data/upcoming_trips.dart';
-import 'package:green/firebase.dart';
-import 'package:green/firebase_options.dart';
 import 'package:green/model/destination_model.dart';
-import 'package:green/model/upcoming_trip_model.dart';
-import 'package:green/navigation.dart';
-import 'package:green/providers/accommodation_provider.dart';
-import 'package:green/providers/activity_provider.dart';
-import 'package:green/providers/destination_provider.dart';
-import 'package:green/providers/flight_detail_provider.dart';
-import 'package:green/providers/tour_provider.dart';
+import 'package:green/presets/colors.dart';
+import 'package:green/provider/begreen_post_provider.dart';
+import 'package:green/provider/user_provider.dart';
 import 'package:green/screens/chatbot_screen.dart';
-import 'package:green/screens/checkout/checkout_screen.dart';
 import 'package:green/screens/destination/destination_details_screen.dart';
-import 'package:green/screens/destination/destination_screen.dart';
 import 'package:green/screens/eastwest_stats_screen.dart';
-import 'package:green/screens/explore/explore_destinations_screen.dart';
-import 'package:green/screens/explore/explore_detail_screen.dart';
 import 'package:green/screens/home_screen.dart';
 import 'package:green/screens/itinerary/saved_itinerary_screen.dart';
 import 'package:green/screens/itinerary/swipe_itinerary_screen.dart';
 import 'package:green/screens/landing/landing_screen.dart';
 import 'package:green/screens/landing/landing_screen1.dart';
 import 'package:green/screens/itinerary/my_itinerary_screen.dart';
-import 'package:green/screens/begreen_community_screen.dart';
+import 'package:green/screens/begreen_item.dart';
 import 'package:green/screens/profile_screen.dart';
+import 'package:camera/camera.dart';
 import 'package:green/screens/landing/question_screen.dart';
 import 'package:green/data/questions.dart';
 import 'package:green/screens/upcomingtrip_details_screen.dart';
-import 'package:green/widgets/flight_card.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+
+List<CameraDescription>? cameras = [];
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  cameras = await availableCameras();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -47,6 +46,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(
+          value: UserProvider(),
+        ),
+        ChangeNotifierProvider.value(
+          value: BeGreenProvider(),
+        ),
         ChangeNotifierProvider.value(
           value: DestinationProvider(),
         ),
@@ -64,18 +69,14 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        // home: DestinationDetailsScreen(
-        //   detail: flightDetailList[0],
-        // ),
-        // home: UpcomingTripDetailsScreen(
-        //   upcomingTrip: upcomingTripList[0],
-        // ),
-        // home: QuestionsScreen(
-        //   questions: questionsList,
-        // ),
-        // home: BeGreenCommunityScreen(),
-        // home: LandingScreen1(),
-        home: HomeScreen(),
+        home: const AuthWidgetTree(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: AppColor
+              .btnColorPrimary, // Change this to your desired primary color
+          fontFamily:
+              'LeagueSpartan', // Change this to your desired font family
+        ),
       ),
     );
   }
